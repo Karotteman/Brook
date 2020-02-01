3bcdcb23cededdc1f1c21dba5120b5d08a9132f7
Karotteman <richer@intechinfo.fr>
2020-02-01 05:10:18 -0500

    Add Player Mouvement

---
+++ Assets/Scripts/PlayerMouvement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+
+    [Header("Player Motor")]
+    //[Range(1f, 15f)]
+    public float walkSpeed;
+
+    CharacterController characterController;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePlayerControls();
+    }
+
+    void HandlePlayerControls()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(hInput, 0, vInput);
+
+        if(direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+        characterController.SimpleMove(direction * walkSpeed * Time.deltaTime);
+
+    }
+}
