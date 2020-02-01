ded461c7ba267d4eef7c3a9798b209a2ba95f291
Fr Blanchet <frblanchet31@gmail.com>
2020-02-01 17:43:52 -0500

    add range to spawnPoint script

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
+    [Range(-10, 10)]
     public float hautX;
+    [Range(-10, 10)]
     public float hautY;
 
+    [Range(-10, 10)]
     public float droitX;
+    [Range(-10, 10)]
     public float droitY;
 
+    [Range(-10, 10)]
     public float basX;
+    [Range(-10, 10)]
     public float basY;
 
+    [Range(-10, 10)]
     public float gaucheX;
+    [Range(-10, 10)]
     public float gaucheY;
 
     // Start is called before the first frame update
